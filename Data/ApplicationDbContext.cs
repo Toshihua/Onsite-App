c43ef87161a97edbd6df180c70bb3b0bf209f1a6
Toshihua <tanawanjoshua28@gmail.com>
2026-04-07 22:18:31 +0800

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Onsite_App.Models.Entities;
+
+namespace Onsite_App.Data
+{
+    public class ApplicationDbContext : DbContext 
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        
+        }
+
+        public DbSet <Student> Students { get; set; }
+    }
+}
